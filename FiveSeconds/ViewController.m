afdaaa454a3bcc3bdd3880aaee9d4e476ac9cee2
Sriram Panyam <sri.panyam@gmail.com>
2016-11-08 19:15:56 -0800

    Capture working baby

--- FiveSeconds/ViewController.m
+++ FiveSeconds/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <CoreMedia/CoreMedia.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <ImageIO/ImageIO.h>
 
 @interface ViewController ()
+
+@property (nonatomic, strong) AVCaptureStillImageOutput *stillImageOutput;
+@property (nonatomic, strong) AVCaptureConnection *videoConnection;
+@property (nonatomic, strong)     AVCaptureSession *captureSession;
 
 @end
 
@@ -22,6 +30,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self initializeCamera];
+}
+
+-(void)initializeCamera {
+    self.captureSession = [[AVCaptureSession alloc] init];
+    self.captureSession.sessionPreset = AVCaptureSessionPresetMedium;
+
+    AVCaptureDevice *frontCamera = nil;
+    NSArray *devices = [AVCaptureDevice devices];
+    for (AVCaptureDevice *device in devices) {
+        NSLog(@"Device name: %@", [device localizedName]);
+        if ([device hasMediaType:AVMediaTypeVideo]) {
+            if ([device position] == AVCaptureDevicePositionFront) {
+                frontCamera = device;
+                break;
+            }
+        }
+    }
+    if (frontCamera) {
+        if ([frontCamera isFocusModeSupported:AVCaptureFocusModeContinuousAutoFocus]) {
+            CGPoint autofocusPoint = CGPointMake(0.5f, 0.5f);
+            NSError *error = nil;
+            if ([frontCamera lockForConfiguration:&error]) {
+                if (error) {
+                }
+                [frontCamera setFocusPointOfInterest:autofocusPoint];
+                [frontCamera setFocusMode:AVCaptureFocusModeContinuousAutoFocus];
+                if (frontCamera.hasFlash) {
+                    frontCamera.flashMode = AVCaptureFlashModeOff;
+                }
+                [frontCamera unlockForConfiguration];
+            }
+        }
+    }
+    
+    // Attach device to session
+    NSError *error;
+    AVCaptureDeviceInput *captureDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:frontCamera error:&error];
+    if (captureDeviceInput) {
+        // Handle the error appropriately.
+        if ([self.captureSession canAddInput:captureDeviceInput]) {
+            [self.captureSession addInput:captureDeviceInput];
+        }
+    }
+    
+    self.videoConnection = nil;
+    self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = @{ AVVideoCodecKey : AVVideoCodecJPEG};
+    [self.stillImageOutput setOutputSettings:outputSettings];
+    if ([self.captureSession canAddOutput:self.stillImageOutput]) {
+        [self.captureSession addOutput:self.stillImageOutput];
+        
+        // Add inputs and outputs.
+        [self.captureSession startRunning];
+    }
 }
 
 
@@ -31,12 +95,41 @@
 }
 
 -(IBAction)takePhoto:(id)sender {    
-    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
-    picker.delegate = self;
-    picker.allowsEditing = YES;
-    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+//    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+//    picker.delegate = self;
+//    picker.allowsEditing = YES;
+//    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+//    picker.showsCameraControls = NO;
+//    
+//    [self presentViewController:picker animated:YES completion:NULL];
     
-    [self presentViewController:picker animated:YES completion:NULL];
+    for (AVCaptureConnection *connection in self.stillImageOutput.connections) {
+        for (AVCaptureInputPort *port in [connection inputPorts]) {
+            if ([[port mediaType] isEqual:AVMediaTypeVideo] ) {
+                self.videoConnection = connection;
+                break;
+            }
+        }
+        if (self.videoConnection) { break; }
+    }
+
+    __weak typeof(self) weakSelf = self;
+    [self.stillImageOutput captureStillImageAsynchronouslyFromConnection:self.videoConnection completionHandler:^(CMSampleBufferRef imageSampleBuffer, NSError *error) {
+         CFDictionaryRef exifAttachments = CMGetAttachment(imageSampleBuffer, kCGImagePropertyExifDictionary, NULL);
+         if (exifAttachments) {
+             // Do something with the attachments.
+         }
+         // Continue as appropriate.
+     }];
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self dummyFunction];
+    });
+    NSLog(@"Here...");
+}
+
+-(void)dummyFunction {
+    NSLog(@"After 2 seconds");
 }
 
 -(IBAction)startVideo:(id)sender {
